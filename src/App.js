c6357f35884d39410e29972323ac9191e21369ab
FarzanaChowdhury <sumaiya.farzana.eleen@gmail.com>
2024-01-10 02:53:44 +0600

    Add tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [taskList, setTaskList] = useState([]);
+  // To get the task
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  }
+  const addTask = () => {
+    const newTaskList = [...taskList, newTask];
+    setTaskList(newTaskList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='addTask'>
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+
+      </div>
+      
+      <div className='list'>
+        {taskList.map((task)=>{
+          return(
+            <h2>{task}</h2>
+          )
+        })}
+      </div>
     </div>
   );
 }
