ff193df797700824844f611bb3775dbe7cd72523
FarzanaChowdhury <sumaiya.farzana.eleen@gmail.com>
2024-02-10 01:45:12 +0600

    update Task

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
+// App.js
 import './App.css';
 import { useState } from 'react';
-import {DeleteTask} from './Task'
+import { ChangeTask } from './Task';
+
 function App() {
-  // To make taskList
   const [taskList, setTaskList] = useState([]);
-  // To get the task
   const [newTask, setNewTask] = useState("");
 
-  //To set the task
   const handleChange = (event) => {
     setNewTask(event.target.value);
   }
 
   const addTask = () => {
-// create task object
-    const task={
-      id : taskList.length === 0 ? 1 : taskList[taskList.length - 1].id +1,
-      taskName : newTask,
+    const task = {
+      id: taskList.length === 0 ? 1 : taskList[taskList.length - 1].id + 1,
+      taskName: newTask,
     }
-    if(newTask !== "")
-    setTaskList([...taskList, task]);
+    if (newTask !== "") {
+      setTaskList([...taskList, task]);
+      setNewTask("");
+    }
   }
+
   const deleteTask = (taskID) => {
-    // Loop through each task,if task == taskname, then false->it will not be in the list. otherwise true
-    const taskListAfterDeletetion = taskList.filter((task) => task.id !== taskID)
-    setTaskList(taskListAfterDeletetion);
+    const taskListAfterDeletion = taskList.filter((task) => task.id !== taskID);
+    setTaskList(taskListAfterDeletion);
   }
 
-  const updateTask = (task_Name) => {
-
+  const updateTask = (taskID, updatedTaskName) => {
+    //"OK" button is clicked
+    const updatedTasks = taskList.map((task) =>
+      task.id === taskID ? { ...task, taskName: updatedTaskName } : task
+    );
+    setTaskList(updatedTasks);
   }
 
   return (
     <div className="App">
       <div className='addTask'>
-        <input onChange={handleChange} />
+        <input value={newTask} onChange={handleChange} />
         <button onClick={addTask}>Add Task</button>
-
       </div>
 
       <div className='list'>
-        {taskList.map((task) => {
-          return  <DeleteTask taskName = {task.taskName} id = {task.id} deleteTask = {deleteTask} updateTask = {updateTask}/>;
-        
-        })}
+        {taskList.map((task) => (
+          <ChangeTask key={task.id} taskName={task.taskName} id={task.id} deleteTask={deleteTask} updateTask={updateTask} />
+        ))}
       </div>
     </div>
   );
--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,46 @@
-export const DeleteTask = (props) =>{
-return(
-        <div>
-              <h2>{props.taskName}</h2>
-              {/* If we have arguments, we need to call a function that returns that func */}
-              <button onClick={() => props.deleteTask(props.id)}> X </button>
-              <button onClick={() => props.updateTask(props.taskName)}>Edit</button>
-        </div>
-)
-}
+// Task.js
+import React, { useState } from 'react';
 
+export const ChangeTask = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedTaskName, setUpdatedTaskName] = useState(props.taskName);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  }
+
+  const handleOkClick = () => {
+    // Update the task with the new task name
+    props.updateTask(props.id, updatedTaskName);
+    setIsEditing(false);
+  }
+
+  const handleCancelClick = () => {
+    // Cancel editing, revert to the original task name
+    setUpdatedTaskName(props.taskName);
+    setIsEditing(false);
+  }
+
+  const handleInputChange = (event) => {
+    // Update the updatedTaskName state when the input changes
+    setUpdatedTaskName(event.target.value);
+  }
+
+  return (
+    <div>
+      {isEditing ? (
+        <div>
+          <input value={updatedTaskName} onChange={handleInputChange} />
+          <button onClick={handleOkClick}>OK</button>
+          <button onClick={handleCancelClick}>Cancel</button>
+        </div>
+      ) : (
+        <div>
+          <h2>{props.taskName}</h2>
+          <button onClick={() => props.deleteTask(props.id)}> X </button>
+          <button id="Edit" onClick={handleEditClick}>Edit</button>
+        </div>
+      )}
+    </div>
+  );
+}
